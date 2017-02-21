a74b93a46fe3a476b00debe49fabf9979db47926
shikimoon <kyy4742488@gmail.com>
2017-02-21 23:08:36 +0800

    kmeans算法

---
+++ kMeans/kMeans.py
@@ -0,0 +1,100 @@
+from numpy import *
+import matplotlib.pyplot as plt
+
+def loadDataSet(fileName):
+    dataMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = list(map(float, curLine))
+        dataMat.append(fltLine)
+    return dataMat
+
+def distEclud(vecA, vecB):
+    return sqrt(sum(pow(array(vecA) - array(vecB), 2)))
+
+def randCent(dataSet, k):
+    n = shape(dataSet)[1]
+    centroids = mat(zeros((k, n)))
+    for j in range(n):
+        minJ = min(dataSet[:, j])
+        rangeJ = float(max(dataSet[:, j]) - minJ)
+        centroids[:, j] = minJ + rangeJ * random.rand(k, 1)
+    return centroids
+
+def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m,2)))
+    centroids = createCent(dataSet, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):
+            minDist = inf; minIndex = -1
+            for j in range(k):
+                distJI = distMeas(centroids[j,:], dataSet[i,:])
+                if distJI < minDist:
+                    minDist = distJI; minIndex = j
+            if clusterAssment[i,0] != minIndex: clusterChanged = True
+            clusterAssment[i,:] = minIndex,minDist ** 2
+        for cent in range(k):
+            ptsInClust = dataSet[nonzero(clusterAssment[:,0].A==cent)[0]]
+            centroids[cent,:] = mean(ptsInClust, axis=0)
+    return centroids, clusterAssment
+
+def biKmeans(dataSet, k, distMeas = distEclud):
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m, 2)))
+    centroid0 = mean(dataSet, axis = 0).tolist()
+    centList = [centroid0]
+    for j in range(m):
+        clusterAssment[j, 1] = distMeas(mat(centroid0), dataSet[j, :]) ** 2
+    while (len(centList) < k):
+        lowestSSE = inf
+        for i in range(len(centList)):
+            ptsInCurrCluster = dataSet[nonzero(clusterAssment[:, 0].A == i)[0], :]
+            centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)
+            sseSplit = sum(splitClustAss[:, 1])
+            sseNotSplit = sum(clusterAssment[nonzero(clusterAssment[:, 0].A != i)[0], 1])
+            if (sseSplit + sseNotSplit) < lowestSSE:
+                bestCentToSplit = i
+                bestNewCents = centroidMat
+                bestClustAss = splitClustAss.copy()
+                lowestSSE = sseSplit + sseNotSplit
+        bestClustAss[nonzero(bestClustAss[:, 0].A == 1)[0], 0] = len(centList)
+        bestClustAss[nonzero(bestClustAss[:, 0].A == 0)[0], 0] = bestCentToSplit
+        centList[bestCentToSplit] = bestNewCents[0, :].flatten()
+        centList.append(bestNewCents[1, :].flatten())
+        clusterAssment[nonzero(clusterAssment[:, 0].A == bestCentToSplit)[0], :] = bestClustAss
+    centList = [convert(i) for i in centList]
+    return mat(centList), clusterAssment
+
+def convert(i):
+    return array(i)[0].tolist()
+
+def plot(dataSet, k, centroids, clusterAssment):
+    numSamples, dim = dataSet.shape
+    if dim != 2:
+        print("Sorry! I can not draw because the dimension of your data is not 2!")
+        return 1
+    mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']
+    if k > len(mark):
+        print("Sorry! Your k is too large! please contact Zouxy")
+        return 1
+    for i in range(numSamples):
+        markIndex = int(clusterAssment[i, 0])
+        plt.plot(dataSet[i, 0], dataSet[i, 1], mark[markIndex])
+
+    mark = ['Dr', 'Db', 'Dg', 'Dk', '^b', '+b', 'sb', 'db', '<b', 'pb']
+    for i in range(k):
+        plt.plot(centroids[i, 0], centroids[i, 1], mark[i], markersize = 12)
+
+    plt.show()
+
+def test():
+    k = 3
+    datMat = mat(loadDataSet('testSet2.txt'))
+    centList,myNewAssments = biKmeans(datMat,k)
+    plot(datMat, k, centList, myNewAssments)
+
+test()
